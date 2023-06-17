15a2144fb0e2cb21ed028c6558828cbb2d4ab375
Axel De Acetis <20586054+adeacetis@users.noreply.github.com>
2023-06-17 17:54:25 +0200

    Copy .prettierrc from aggr-ui

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  printWidth: 140,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
+  endOfLine: 'auto',
+  trailingComma: 'none',
+  arrowParens: 'avoid'
 }
