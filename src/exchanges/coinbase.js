5ead2adad56ad4768d73557ea110146c0c4c1ec5
Tucsky <tucsky@pm.me>
2024-06-11 15:44:54 +0200

    Feat(coinbase): add support for -INTX markets

--- src/exchanges/coinbase.js
+++ src/exchanges/coinbase.js
@@ -1,6 +1,7 @@
 const Exchange = require('../exchange')
 const axios = require('axios')
 const { getHms, sleep } = require('../helper')
+const INTX_PAIR_REGEX = /-INTX$/
 
 class Coinbase extends Exchange {
   constructor() {
@@ -9,16 +10,45 @@ class Coinbase extends Exchange {
     this.id = 'COINBASE'
 
     this.endpoints = {
-      PRODUCTS: 'https://api.pro.coinbase.com/products'
+      PRODUCTS: [
+        'https://api.pro.coinbase.com/products',
+        'https://api.coinbase.com/api/v3/brokerage/market/products?product_type=FUTURE&contract_expiry_type=PERPETUAL'
+      ]
     }
 
-    this.url = 'wss://ws-feed.pro.coinbase.com'
+    this.url = pair => {
+      if (INTX_PAIR_REGEX.test(pair)) {
+        return 'wss://advanced-trade-ws.coinbase.com'
+      }
+
+      return 'wss://ws-feed.pro.coinbase.com'
+    }
   }
 
-  formatProducts(data) {
-    return data
-      .filter(product => product.status === 'online')
-      .map(product => product.id)
+  formatProducts(response) {
+    const products = []
+
+    const [spotResponse, perpResponse] = response
+
+    if (spotResponse && spotResponse.length) {
+      for (const product of spotResponse) {
+        if (product.status !== 'online') {
+          continue
+        }
+
+        products.push(product.id)
+      }
+    }
+
+    if (perpResponse && perpResponse.products && perpResponse.products.length) {
+      for (const product of perpResponse.products) {
+        products.push(product.product_id)
+      }
+    }
+
+    return {
+      products
+    }
   }
 
   /**
@@ -31,10 +61,19 @@ class Coinbase extends Exchange {
       return
     }
 
+    const isIntx = INTX_PAIR_REGEX.test(pair)
+
     api.send(
       JSON.stringify({
         type: 'subscribe',
-        channels: [{ name: 'matches', product_ids: [pair] }]
+        ...(isIntx
+          ? {
+              channel: 'market_trades',
+              product_ids: [pair]
+            }
+          : {
+              channels: [{ name: 'matches', product_ids: [pair] }]
+            })
       })
     )
   }
@@ -49,10 +88,19 @@ class Coinbase extends Exchange {
       return
     }
 
+    const isIntx = INTX_PAIR_REGEX.test(pair)
+
     api.send(
       JSON.stringify({
         type: 'unsubscribe',
-        channels: [{ name: 'matches', product_ids: [pair] }]
+        ...(isIntx
+          ? {
+              channel: 'market_trades',
+              product_ids: [pair]
+            }
+          : {
+              channels: [{ name: 'matches', product_ids: [pair] }]
+            })
       })
     )
   }
@@ -60,8 +108,23 @@ class Coinbase extends Exchange {
   onMessage(event, api) {
     const json = JSON.parse(event.data)
 
-    if (json && json.type === 'match') {
-      return this.emitTrades(api.id, [this.formatTrade(json, json.product_id)])
+    if (json) {
+      if (json.type === 'match') {
+        return this.emitTrades(api.id, [
+          this.formatTrade(json, json.product_id)
+        ])
+      } else if (json.channel === 'market_trades') {
+        return this.emitTrades(
+          api.id,
+          json.events.reduce((acc, event) => {
+            if (event.type === 'update') {
+              acc.push(...event.trades.map(trade => this.formatTrade(trade, trade.product_id)))
+            }
+
+            return acc
+          }, [])
+        )
+      }
     }
   }
 
@@ -72,14 +135,26 @@ class Coinbase extends Exchange {
       timestamp: +new Date(trade.time),
       price: +trade.price,
       size: +trade.size,
-      side: trade.side === 'buy' ? 'sell' : 'buy'
+      side: trade.side === 'buy' || trade.side === 'BUY' ? 'sell' : 'buy'
     }
   }
 
   async getMissingTrades(range, totalRecovered = 0, fromTradeId) {
-    const endpoint = `https://api.exchange.coinbase.com/products/${
-      range.pair
-    }/trades?limit=1000${fromTradeId ? '&after=' + fromTradeId : ''}`
+    let endpoint
+
+    const isIntx = INTX_PAIR_REGEX.test(range.pair)
+
+    if (isIntx) {
+      endpoint = `https://api.coinbase.com/api/v3/brokerage/market/products/${
+        range.pair
+      }/ticker?limit=100&end=${Math.round(range.to / 1000)}&start=${Math.round(
+        range.from / 1000
+      )}`
+    } else {
+      endpoint = `https://api.exchange.coinbase.com/products/${
+        range.pair
+      }/trades?limit=1000${fromTradeId ? '&after=' + fromTradeId : ''}`
+    }
 
     if (+new Date() - range.to < 10000) {
       // coinbase api lags a lot
@@ -90,14 +165,21 @@ class Coinbase extends Exchange {
     return axios
       .get(endpoint)
       .then(response => {
-        if (response.data.length) {
+        if (isIntx) {
+          return response.data.trades
+        }
+
+        return response.data
+      })
+      .then(data => {
+        if (data.length) {
           const earliestTradeId =
-            response.data[response.data.length - 1].trade_id
+            data[data.length - 1].trade_id
           const earliestTradeTime = +new Date(
-            response.data[response.data.length - 1].time
+            data[data.length - 1].time
           )
 
-          const trades = response.data
+          const trades = data
             .map(trade => this.formatTrade(trade, range.pair))
             .filter(
               a => a.timestamp >= range.from + 1 && a.timestamp < range.to
